f4a9ae4297631ef5f623b8bca86c9dd54b014662
andrew <andrewliu13531@gmail.com>
2024-08-28 15:00:17 -0700

    add keyboard support

--- calculator.js
+++ calculator.js
@@ -19,16 +19,16 @@ const divide = function(num1, num2) {
 function operate(num1, num2, operator) {
     let solution = 0;
 
-    if(operator == "add") {
+    if(operator == "+") {
         solution = add(parseFloat(num1), parseFloat(num2));
     }
-    else if(operator == "sub") {
+    else if(operator == "-") {
         solution = subtract(parseFloat(num1), parseFloat(num2));
     }
-    else if(operator == "multi") {
+    else if(operator == "*") {
         solution = multiply(parseFloat(num1), parseFloat(num2));
     }
-    else if(operator == "divide") {
+    else if(operator == "/") {
         solution = divide(parseFloat(num1), parseFloat(num2));
     }
     solution = Math.round(solution * 100) / 100;
@@ -39,8 +39,7 @@ function operate(num1, num2, operator) {
 function displayNum(num) {
 
     if(num == ".") {
-        const decimal = document.querySelector("#decimal");
-        decimal.disabled = true;
+        disableDecimalButton(true);
     }
 
     if(currentOperator != "") {
@@ -60,8 +59,7 @@ function calculatorOperator(operator) {
         display("ERROR");
     }
     else {
-        const decimal = document.querySelector("#decimal");
-        decimal.disabled = false;
+        disableDecimalButton(false);
 
         if(currentOperator == "divide" && secondNum == "0") {
             clearAll();
@@ -96,8 +94,7 @@ function equals() {
 }
 
 function clearAll() {
-    const decimal = document.querySelector("#decimal");
-    decimal.disabled = false;
+    disableDecimalButton(false);
     firstNum = "";
     secondNum = "";
     currentOperator = "";
@@ -112,11 +109,41 @@ function display(text) {
 
 function backSpace() {
     if(secondNum != "") {
+        if(secondNum.slice(-1) == ".") {
+            disableDecimalButton(false);
+        }
         secondNum = secondNum.substring(0, secondNum.length - 1);
         display(secondNum);
     }
     else if(typeof firstNum == "string") {
+        if(firstNum.slice(-1) == ".") {
+            disableDecimalButton(false);
+        }
         firstNum = firstNum.substring(0, firstNum.length - 1);
         display(firstNum);
     }
 }
+
+function disableDecimalButton(bool) {
+    const decimal = document.querySelector("#decimal");
+
+    decimal.disabled = bool;
+}
+
+document.addEventListener('keydown', function(event) {
+    if(!isNaN(event.key) || event.key == ".") {
+        displayNum(event.key);
+    }
+    else if(event.key == "+" || event.key == "-" || event.key == "*" || event.key == "/") {
+        calculatorOperator(event.key);
+    }
+    else if(event.key == "=" || event.key == "Enter") {
+        equals();
+    }
+    else if(event.key == "Backspace") {
+        backSpace();
+    }
+    else if(event.key == "Escape") {
+        clearAll();
+    }
+});
